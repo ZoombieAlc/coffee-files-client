22fd097df8b7c66a5e60468ff0d96f7165df36dd
ZoombieAlc <zoomiealc@gmail.com>
2024-06-26 00:51:59 -0500

    add react icons and colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
-import { useState } from "react";
 import "./App.css";
+import {
+  FolderIcon,
+  FolderOpenIcon,
+  ServerIcon,
+  TrashIcon,
+} from "@heroicons/react/16/solid";
 
 function App() {
   return (
     <>
-      <p>Hola</p>
+      <p className="font-inter font-medium">Hola</p>
+      <TrashIcon className=" text-coffee_text_pale_blue" />
+      <FolderOpenIcon className="text-coffee_violet_dark" />
+      <FolderIcon className="text-coffee_violet_dark" />
+      <ServerIcon className=" text-coffee_violet_dark" />
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        coffee_violet_dark: "#2E2B55",
+        coffee_violet_light: "#7448AF",
+        coffee_violet_darker: "#232245",
+        coffee_green_aqua: "#00AAAA",
+        coffee_text_pale_blue: "#B3EBF1",
+      },
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+      },
+      backgroundImage: {
+        wallpaper1: "url('src/assets/images/wallpaper1.jpg')",
+      },
+    },
   },
   plugins: [],
 };
