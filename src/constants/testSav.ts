2e2bcca23b3165aab1659e8805162b248f1296bc
Luis Bazan <luis@bazan.pe>
2024-06-25 23:43:55 -0500

    Added testSav file

---
+++ src/constants/testSav.ts
@@ -0,0 +1,80 @@
+export const testTheme = {
+  name: "Golden Sunrise",
+  colors: {
+    primary: "#f1c40f",
+    secondary: "#f39c12",
+    background: "#f9e79f",
+    text: "#000000",
+  },
+  opacity: 0.8,
+  background_gradient: "linear-gradient(135deg, #f1c40f, #f39c12)",
+  background_image: "../golden_sunrise.jpg",
+  background_size: "cover",
+  background_opacity: 1,
+};
+
+/*
+Themes Names:
+Frosted Silk - Midnight Blue
+Snow Serenity - Onyx Black
+Raven Lux - Emerald Green
+Garnet Flame - Misty Rose
+Golden Sunrise - Deep Teal
+Honey Glow - Forest Green
+Emerald Luxe - Ivory White
+Amethyst Whisper - Light gray
+Violet Dream - Midnight Blue
+*/
+
+export const testFile1 = {
+  name: "testFile1.txt",
+  content: "This is a test file.",
+  created: 1719375076000,
+  last_modified: 1719375076000,
+  size: 20,
+  type: "txt",
+  path: "A:/Documents/testFile1.txt",
+};
+
+export const testFile2 = {
+  name: "testFile2.txt",
+  content: "This is a test file.",
+  created: 1719375076000,
+  last_modified: 1719375076000,
+  size: 20,
+  type: "txt",
+  path: "A:/Documents/testFile2.txt",
+};
+
+export const testSav = {
+  settings: {
+    theme: "A:/themes/golden_sunrise.json",
+  },
+  disks: [
+    {
+      name: "A", // A-Z
+      limitSize: 1000000000000, // 1TB
+      occupiedSize: 32000000000, // 32GB
+      root: {
+        name: "root",
+        created: 1719375076000, // Milliseconds epoch
+        last_modified: 1719375076000, // Milliseconds epoch
+        size: 0, // 0 bytes
+        path: "A:/",
+        isRoot: true,
+        folders: [
+          {
+            name: "Documents",
+            created: 1719375076000,
+            last_modified: 1719375076000,
+            size: 0,
+            path: "A:/Documents",
+            folders: [],
+            files: [],
+          },
+        ],
+        files: [testFile1],
+      },
+    },
+  ],
+};
