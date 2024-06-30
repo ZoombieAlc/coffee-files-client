f1b08ad5af292b9d1d58414759221172db14c5d8
ZoombieAlc <zoomiealc@gmail.com>
2024-06-30 17:24:01 -0500

    drag and drop to multiple selection

--- src/components/organisms/itemPanel/index.tsx
+++ src/components/organisms/itemPanel/index.tsx
@@ -12,28 +12,37 @@ import { Folder, GenericFile } from "../../../types";
 
 import ItemFolder from "../../molecules/itemFolder";
 import ItemFile from "../../molecules/itemFile";
+import {
+  DndContext,
+  DragEndEvent,
+  DragOverlay,
+  DragStartEvent,
+} from "@dnd-kit/core";
 
 function ItemPanel() {
   const files: GenericFile[] = [testFile1, testFile2, testFile3, testFile4];
   const folders: Folder[] = [testDirectory1, testDirectory2];
   const [selectedItems, setSelectedItems] = useState<string[]>([]);
+  const [isDragging, setIsDragging] = useState(false);
+  const [draggingItems, setDraggingItems] = useState<string[]>([]);
 
   const { DragSelection } = useSelectionContainer({
-    onSelectionChange: ({ top, left, width, height }) => {
-      const selectedIds = [...document.querySelectorAll(".selectable")]
+    onSelectionChange: (box) => {
+      const selectedIds = Array.from(document.querySelectorAll(".selectable"))
         .filter((element) => {
           const rect = element.getBoundingClientRect();
           return (
-            rect.left < left + width &&
-            rect.right > left &&
-            rect.top < top + height &&
-            rect.bottom > top
+            rect.left < box.left + box.width &&
+            rect.right > box.left &&
+            rect.top < box.top + box.height &&
+            rect.bottom > box.top
           );
         })
         .map((element) => element.getAttribute("data-id") || "");
 
       setSelectedItems(selectedIds);
     },
+    isEnabled: !isDragging,
   });
 
   const handleItemClick = (e: React.MouseEvent, path: string) => {
@@ -50,32 +59,64 @@ function ItemPanel() {
     }
   };
 
+  const handleDragStart = (event: DragStartEvent) => {
+    const draggedItemId = event.active.id.toString(); 
+    setIsDragging(true);
+    if (!selectedItems.includes(draggedItemId)) {
+      setSelectedItems([draggedItemId]);
+      setDraggingItems([draggedItemId]);
+    } else {
+      setDraggingItems(selectedItems);
+    }
+  };
+
+  const handleDragEnd = (event: DragEndEvent) => {
+    const { over } = event;
+    if (over) {
+      const overId = over.id.toString(); 
+      
+      console.log(`Mover ${draggingItems.join(", ")} a ${overId}`);
+      
+    }
+    setIsDragging(false);
+    setDraggingItems([]);
+  };
+
   return (
-    <div className="bg-coffee_violet_dark flex-grow m-4 overflow-auto flex flex-col p-4 rounded-xl relative">
-      <DragSelection />
-      <div className="grid grid-cols-4 text-coffee_violet_light border-b border-coffee_green_aqua pb-2 mb-2">
-        <span>Name</span>
-        <span>Date modified</span>
-        <span>Created date</span>
-        <span>Size</span>
+    <DndContext onDragStart={handleDragStart} onDragEnd={handleDragEnd}>
+      <div className="bg-coffee_violet_dark flex-grow m-4 overflow-auto flex flex-col p-4 rounded-xl relative">
+        <DragSelection />
+        <div className="grid grid-cols-4 text-coffee_violet_light border-b border-coffee_green_aqua pb-2 mb-2">
+          <span>Name</span>
+          <span>Date modified</span>
+          <span>Created date</span>
+          <span>Size</span>
+        </div>
+        {folders.map((folder) => (
+          <ItemFolder
+            key={folder.path}
+            item={folder}
+            isSelected={selectedItems.includes(folder.path)}
+            onClick={handleItemClick}
+          />
+        ))}
+        {files.map((file) => (
+          <ItemFile
+            key={file.path}
+            item={file}
+            isSelected={selectedItems.includes(file.path)}
+            onClick={handleItemClick}
+          />
+        ))}
       </div>
-      {folders.map((folder) => (
-        <ItemFolder
-          key={folder.path}
-          item={folder}
-          isSelected={selectedItems.includes(folder.path)}
-          onClick={handleItemClick}
-        />
-      ))}
-      {files.map((file) => (
-        <ItemFile
-          key={file.path}
-          item={file}
-          isSelected={selectedItems.includes(file.path)}
-          onClick={handleItemClick}
-        />
-      ))}
-    </div>
+      <DragOverlay>
+        {isDragging && draggingItems.length > 0 && (
+          <div className="p-2 rounded-md bg-coffee_violet_light text-coffee_text_pale_blue">
+            {draggingItems.length} items
+          </div>
+        )}
+      </DragOverlay>
+    </DndContext>
   );
 }
 
