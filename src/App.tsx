d553b9820ece9ee601bcad2b45b9cfb48eb209d6
Luis Bazan <luis@bazan.pe>
2024-06-30 00:16:50 -0500

    Session Router :v?

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 import ItemPanel from "./components/organisms/itemPanel";
 import ToolBar from "./components/organisms/toolBar";
+import MainRouter from "./routers";
 
 function App() {
   return (
     <>
-      <div className="flex h-screen w-screen bg-wallpaper1">
+      <MainRouter>
+        {/* <div className="flex h-screen w-screen bg-wallpaper1">
         <div className="bg-white-100 rounded-md bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-0 border border-coffee_violet_dark h-screen w-96 mx-1 my-2"></div>
         <div className="flex flex-col w-full">
           <ToolBar />
           <ItemPanel />
         </div>
-      </div>
+      </div> */}
+        <div></div>
+      </MainRouter>
     </>
   );
 }
