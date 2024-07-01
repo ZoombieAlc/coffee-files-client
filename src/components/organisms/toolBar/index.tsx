2ab1d49fb801485c1d53723665b46d426c954fbe
Luis Bazán <luis@bazan.pe>
2024-06-30 21:12:36 -0500

    Modify Toolbar to use path, and move tree buttons

--- src/components/organisms/toolBar/index.tsx
+++ src/components/organisms/toolBar/index.tsx
@@ -1,7 +1,7 @@
 import {
-  ArrowLeftIcon,
   ArrowPathIcon,
   ArrowRightIcon,
+  ArrowUpIcon,
   DocumentDuplicateIcon,
   PlusIcon,
   ScissorsIcon,
@@ -9,8 +9,20 @@ import {
 } from "@heroicons/react/16/solid";
 import { PencilIcon } from "@heroicons/react/24/outline";
 import { ClipboardDocumentIcon } from "@heroicons/react/24/outline";
+import useSavFile from "../../../hooks/useSavFile";
 
 function ToolBar() {
+  const { currentFolder, syncSystem, navigateTo } = useSavFile();
+
+  const handleSync = () => {
+    syncSystem();
+  };
+
+  const handleUp = () => {
+    if (currentFolder?.path.split("\\").length === 1) return;
+    navigateTo(currentFolder?.path.split("\\").slice(0, -1).join("\\") ?? "");
+  };
+
   return (
     <div className="m-2 p-2 bg-coffee_violet_darker w-full flex flex-col rounded-md gap-2 font-inter text-coffee_text_pale_blue">
       <div className="p-2 font-medium text-sm flex flex-row justify-start sm:justify-evenly gap-6 sm:w-3/5 overflow-x-auto">
@@ -54,20 +66,23 @@ function ToolBar() {
 
       <div className="flex flex-row justify-between items-center gap-4 px-2 mt-2">
         <div className="flex gap-2">
-          <button className="p-1 rounded hover:bg-coffee_violet_light">
-            <ArrowLeftIcon className="w-6" />
+          <button
+            className="p-1 rounded hover:bg-coffee_violet_light"
+            onClick={handleUp}
+          >
+            <ArrowUpIcon className="w-6" />
           </button>
-          <button className="p-1 rounded hover:bg-coffee_violet_light">
-            <ArrowRightIcon className="w-6" />
-          </button>
-          <button className="p-1 rounded hover:bg-coffee_violet_light">
+          <button
+            className="p-1 rounded hover:bg-coffee_violet_light"
+            onClick={handleSync}
+          >
             <ArrowPathIcon className="w-6" />
           </button>
         </div>
 
         <div className="flex flex-grow items-center gap-4">
           <div className="flex-grow bg-coffee_violet_light rounded-md px-2 py-1 flex items-center">
-            C:/Folder1/SubFolder1/
+            {currentFolder?.path}
           </div>
           <input
             placeholder="Search..."
